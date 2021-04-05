3f678ff58b724521415de1938c05f77f539b9bab
alisharaj <alisharaj2607@gmail.com>
2021-04-05 19:04:24 +0530

    created our first APIVAIEW

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
+        an_apiview = [
+            'Uses HTTP methods..',
+            'line2',
+            'line3',
+            'line4'
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview': an_apiview})
